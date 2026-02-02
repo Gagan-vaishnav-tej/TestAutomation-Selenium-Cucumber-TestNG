0337d3699d331dc10079079db8b2827dd6c283ad
Gagan-vaishnav-tej <gaganvegireddy@gmail.com>
2026-02-02 21:43:43 +0530

    Types of Browser Waits and handling Synchronization

---
+++ Selenium/Practical/SeleniumBasics/src/test/java/sychronization/ExplicitWait.java
@@ -0,0 +1,32 @@
+package sychronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		
+		WebElement textField =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']")));
+		WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password']")));
+		textField.sendKeys("Admin");
+		password.sendKeys("admin123");
+		
+		WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+		login.click();
+		driver.quit();
+
+	}
+
+}
---
+++ Selenium/Practical/SeleniumBasics/src/test/java/sychronization/ImplicitWait.java
@@ -0,0 +1,23 @@
+package sychronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		
+		driver.quit();
+
+	}
+
+}
