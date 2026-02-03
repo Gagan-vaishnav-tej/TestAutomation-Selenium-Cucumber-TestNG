067af8f92a7f05e1367496d8b9738ba67344966f
Gagan-vaishnav-tej <gaganvegireddy@gmail.com>
2026-02-03 10:41:43 +0530

    Browser Navigation methods

---
+++ Selenium/Practical/SeleniumBasics/src/test/java/webdriverMethods/NavigationMethods.java
@@ -0,0 +1,40 @@
+package webdriverMethods;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationMethods {
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		driver.get("https://demo.nopcommerce.com/");
+		
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		driver.navigate().to(new URL("https://demo.nopcommerce.com/"));
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().back();
+//		Thread.sleep(8000);
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().forward();
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.navigate().refresh();
+		
+		driver.quit();
+		
+		
+	}
+
+}
