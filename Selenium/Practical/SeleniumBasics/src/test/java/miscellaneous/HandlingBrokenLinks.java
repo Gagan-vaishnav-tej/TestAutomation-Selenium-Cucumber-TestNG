8eea09dcf3288a1704dd43fb6da654db5c2a4cd5
Gagan-vaishnav-tej <gaganvegireddy@gmail.com>
2026-02-10 15:09:27 +0530

    Handling ScreenShots,SVG Elements, Broken links and ShadowDOM

--- Selenium/Practical/SeleniumBasics/src/test/java/miscellaneous/HandlingBrokenLinks.java
+++ Selenium/Practical/SeleniumBasics/src/test/java/miscellaneous/HandlingBrokenLinks.java
@@ -1,6 +1,6 @@
 package miscellaneous;
 
-import java.io.IOException;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandlingBrokenLinks {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.deadlinkcity.com/");
